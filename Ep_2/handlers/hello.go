e7333bcea0b4df194a124ee6bac54701c5db7571
Loyd Ngei <loydngei@gmail.com>
2024-12-10 07:15:03 +0300

    ServeHTTP logging better messages now

--- Ep_2/handlers/hello.go
+++ Ep_2/handlers/hello.go
@@ -16,12 +16,16 @@ func NewHello(l *log.Logger) *Hello {
 }
 
 func (h *Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	h.l.Println("Hello World")
+	h.l.Println("Handle hello requests")
+
+	// Read the body
 	d, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		http.Error(rw, "Ooops!", http.StatusBadRequest)
+		h.l.Println("Error reading body", err)
+
+		http.Error(rw, "Unable to read request body", http.StatusBadRequest)
 		return
 	}
-
+	// write the response
 	fmt.Fprintf(rw, "Hello %s", d)
 }
