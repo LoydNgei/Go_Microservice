5b45714c55306ea38ab911239df726266a7890f1
Loyd Ngei <loydngei@gmail.com>
2024-12-10 07:15:56 +0300

    Added GET and POST methods inside the Products handler

--- Ep_2/handlers/products.go
+++ Ep_2/handlers/products.go
@@ -14,19 +14,33 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
+// ServeHTTP, main entry point for the handler
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		p.getProducts(rw, r)
 		return
 	}
 
+	if r.Method == http.MethodGet {
+		p.addProduct(rw, r)
+		return
+	}
+
 	// catch all
+	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
+// getProducts returns the products from the data store
 func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
+
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
 }
